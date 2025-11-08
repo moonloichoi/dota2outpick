0c3d514311aca7cd5e488eeb6227ef16f0a06e2a
moonloichoi <tuan.do@kobanstudio.com>
2025-11-08 15:33:31 +0700

    Create state.js

---
+++ core/state.js
@@ -0,0 +1,17 @@
+export const MAX = 5;
+
+export const state = {
+  enemyQueue: []
+};
+
+export function loadState() {
+  try {
+    state.enemyQueue = JSON.parse(localStorage.getItem("enemyQueue") || "[]");
+  } catch {
+    state.enemyQueue = [];
+  }
+}
+
+export function saveState() {
+  localStorage.setItem("enemyQueue", JSON.stringify(state.enemyQueue));
+}
